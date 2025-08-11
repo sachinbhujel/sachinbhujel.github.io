1b9a8c0f82332cd0c630b59b13e7493059710fc7
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2025-08-11 13:53:59 +0545

    Initial commit

---
+++ portfolio/src/app/layout.js
@@ -0,0 +1,41 @@
+import { Dongle } from "next/font/google";
+import "./globals.css";
+import Navbar from "./components/Navbar";
+
+const dongleFont = Dongle({
+    variable: "--font-dongle",
+    subsets: ["latin"], // only English
+    weight: ["400"], // only regular weight
+});
+
+export const metadata = {
+    title: "Sachin Bhujel",
+    icons: {
+        icon: [{ url: "/favicon.ico" }],
+    },
+    description: "Portfolio Website",
+};
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en" data-theme="light">
+            <head>
+                <link rel="icon" href="/favicon.ico" />
+                <script
+                    defer
+                    data-website-id="6894d440594ad4c6a4bd72d2"
+                    data-domain="sachinbhujel.netlify.app"
+                    src="https://datafa.st/js/script.js"
+                ></script>
+            </head>
+            <body
+                className={`${dongleFont.variable} antialiased overflow-auto`}
+            >
+                <div className="w-[100%] md:w-[700px] m-auto p-1">
+                    <Navbar />
+                    {children}
+                </div>
+            </body>
+        </html>
+    );
+}
