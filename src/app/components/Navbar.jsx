8775a6f41f7e3a1c53fd0807e6732bdb53ce76c6
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2025-08-12 07:25:10 +0000

    Some Improvement

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar() {
     return (
         <>
             <div className="flex justify-center sticky top-1 items-center gap-[25px] w-full z-10">
-                <div className="w-[60%] sm:w-[80%] border flex items-center sm:justify-center justify-end bg-gray-10/50 backdrop-blur-sm rounded-4xl py-2 px-4">
+                <div className="w-[70%] sm:w-[80%] border flex items-center sm:justify-center justify-end bg-gray-10/50 backdrop-blur-sm rounded-4xl py-2 px-4">
                     <div className="flex items-center sm:gap-8 gap-4">
                         <div className="flex items-center gap-4">
                                     <ul
