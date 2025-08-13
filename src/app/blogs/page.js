1b2c8c601ee56f94102a89493554ee219ee8dd6d
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2025-08-13 14:34:12 +0000

    Error On No Blog Post

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -8,19 +8,23 @@ function page() {
             <div className="not-prose flex flex-col gap-4 p-4">
                 <h2 className="text-2xl font-semibold">Blogs</h2>
                 <div className="flex flex-col gap-5 p-1">
-                    {blogs.map((blog, index) => (
-                        <div
-                            key={index}
-                            className="flex items-center justify-between gap-5"
-                        >
-                            <Link href={blog.link}>
-                                <h2 className="cursor-pointer hover:underline">
-                                    {blog.title}
-                                </h2>
-                            </Link>
-                            <p className="text-sm">{blog.date}</p>
-                        </div>
-                    ))}
+                    {blogs.length > 0 ? (
+                        blogs.map((blog, index) => (
+                            <div
+                                key={index}
+                                className="flex items-center justify-between gap-5"
+                            >
+                                <Link href={blog.link}>
+                                    <h2 className="cursor-pointer hover:underline">
+                                        {blog.title}
+                                    </h2>
+                                </Link>
+                                <p className="text-sm">{blog.date}</p>
+                            </div>
+                        ))
+                    ) : (
+                        <p className="text-center mt-10">No blogs available.</p>
+                    )}
                 </div>
             </div>
         </>
